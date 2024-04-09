426bd8ccbcb6be6b4cecc0a9bd45113c5e9c6940
Liang <chaohualiang12@gmail.com>
2024-04-09 17:09:14 -0400

    add components

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#"> Breakfast</a></li>
+            <li><a href="#"> Lunch</a></li>
+            <li><a href="#"> Dinner</a></li>
+            <li><a href="#">Dessert</a></li>
+            <li><a href="#"> Log In</a></li>
+            <li><a href="#">Sign Up</a></li>
+        </ul>
+        <hr></hr>
+    </nav>
+  )
+}
+
+export default NavBar
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-
+//import App from './App';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+    <Header />
+    <NavBar/>
+    <Footer/>
+    </>
   </React.StrictMode>
 );
