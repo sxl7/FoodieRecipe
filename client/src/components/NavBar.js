3718e872c1e197597e96f7db9e99e4b4708f3ee4
sxl7 <lshuxin4@gmail.com>
2024-04-10 17:42:22 -0400

    Login and Signup

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,20 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Login from './Login';
+import Register from './Register';
 
 function NavBar() {
+  const [viewLogin, setViewLogin] = useState(false);
+  const [viewRegister, setViewRegister] = useState(false);
+  // Function to handle login button click
+  const handleLoginClick = () => {
+    setViewLogin(true);
+  };
+
+  // Function to handle canceling login
+  const handleLoginCancel = () => {
+    setViewLogin(false);
+  };
+  // Function to handle register button click
+  const handleRegisterClick = () => {
+    setViewRegister(true);
+  };
+
+  // Function to handle canceling register
+  const handleRegisterCancel = () => {
+    setViewRegister(false);
+  };
+
   return (
     <nav>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#"> Breakfast</a></li>
-            <li><a href="#"> Lunch</a></li>
-            <li><a href="#"> Dinner</a></li>
-            <li><a href="#">Dessert</a></li>
-            <li><a href="#"> Log In</a></li>
-            <li><a href="#">Sign Up</a></li>
-        </ul>
-        <hr></hr>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Breakfast</a></li>
+        <li><a href="#">Lunch</a></li>
+        <li><a href="#">Dinner</a></li>
+        <li><a href="#">Dessert</a></li>
+        <li><a href onClick={handleLoginClick}>Log In</a></li>
+        {/* Separate modal for login */}
+        {viewLogin && (
+          <div className="loginModal">
+            <div className="modalContent">
+              <Login onCancel={handleLoginCancel} />
+            </div>
+          </div>
+        )}
+        <li><a href onClick={handleRegisterClick}>Sign Up</a></li>
+        {viewRegister && (
+          <div className="loginModal">
+            <div className="modalContent">
+              <Register onCancel={handleRegisterCancel} />
+            </div>
+          </div>
+        )}
+      </ul>
+      <hr />
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
---
+++ client/src/components/Register.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function Register(){
+    return (
+        <div className="login-modal">
+          <div className="modal-content">
+            <form>
+            <div className="form-group">
+                <label className="form-label">Your Name</label>
+                <input type="name" placeholder="Enter your name" />
+              </div>
+              <div className="form-group">
+                <label className="form-label">Email Address</label>
+                <input type="email" placeholder="Enter your email" />
+              </div>
+    
+              <div className="form-group">
+                <label className="form-label">Password</label>
+                <input type="password" placeholder="Enter your password" />
+              </div>
+    
+              <div className="form-group">
+                <label className="form-label">Re-enter Password</label>
+                <input type="password" placeholder="Confirm your password" />
+              </div>
+
+              <button type='submit' className='button'>
+                Submit
+              </button>
+              <button type='cancel' className='button'>
+                Cancel
+              </button>
+            </form>
+          </div>
+        </div>
+      );
+    
+}
+export default Register
