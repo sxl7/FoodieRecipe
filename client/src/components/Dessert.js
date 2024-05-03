3e367578101646d1fd91cb6ef26609249dc4cfc0
sxl7 <lshuxin4@gmail.com>
2024-05-02 23:32:33 -0400

    router

---
+++ client/src/components/Dessert.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Desssert() {
+  return (
+    <div>
+      <h2 style={{marginTop: '80px', marginBottom: '800px', fontSize: '24px'}}>Dessert Page</h2>
+    </div>
+  );
+}
+
+export default Desssert;
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-/* import App from './App'; */
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Body from './components/Body';
+import Lunch from './components/Lunch';
+import Breakfast from './components/Breakfast';
+import Dinner from './components/Dinner';
+import Dessert from './components/Dessert';
 import './style/index.css';
-import BackEndTesting from './BackEndTesting';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+//import BackEndTesting from './BackEndTesting';
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
     <Header />
-    <Body />
+    <Router>
+      <Routes>
+      <Route path="/" element={<Body />} /> 
+        <Route path="/home" element={<Body />} /> 
+        <Route path="/breakfast" element={<Breakfast />} />
+        <Route path="/lunch" element={<Lunch />} />
+        <Route path="/dinner" element={<Dinner />} />
+        <Route path="/dessert" element={<Dessert />} />
+      </Routes>
+    </Router>
     <Footer/>
-    <BackEndTesting/>
+    {/*<BackEndTesting/>*/}
     </>
   </React.StrictMode>
 );
