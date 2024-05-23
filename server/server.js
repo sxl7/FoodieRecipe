540fd68c2c20ab20f7e0d933a0cdff72b3c3c75e
sxl7 <97860367+sxl7@users.noreply.github.com>
2024-05-23 02:27:13 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 const corsOptions = {
-  origin: 'https://foodie-recipe-5cgy.vercel.app', // Replace with your frontend URL
+  origin: 'https://foodie-recipe-5cgy.vercel.app',
 };
 
 app.use(cors(corsOptions));
